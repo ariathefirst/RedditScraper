3ad68187cfdf664e521e403b9df25ea8eb2cc67a
sravyadivakarla <sravyad@gmail.com>
2018-02-28 16:49:12 -0800

    add persective

--- rparser.py
+++ rparser.py
@@ -1,6 +1,29 @@
 import praw
 import datetime
 import csv
+import json
+import googleapiclient
+from googleapiclient import discovery
+
+def runPerspective(str):
+	API_KEY='AIzaSyAlCwhKJ0C8n4eFM-ioPC5-MCFYy4P-TT8'
+
+	# Generates API client object dynamically based on service name and version.
+	service = discovery.build('commentanalyzer', 'v1alpha1', developerKey=API_KEY)
+
+	analyze_request = {
+		'comment': { 'text': str },
+		'requestedAttributes': {'TOXICITY': {}}
+	}
+	try:
+		response = service.comments().analyze(body=analyze_request).execute()
+		json.dumps(response)
+		return response["attributeScores"]["TOXICITY"]["summaryScore"]["value"]
+		# print response.attributeScores.TOXICITY.summaryScore.value
+	except googleapiclient.errors.HttpError as e:
+		return "Error!"	
+
+	
 
 reddit = praw.Reddit(client_id='OlM6d2hKSrhbkw',
                      client_secret='w3kllzs-03WScaa9DFMHvRdTnQg',
@@ -11,17 +34,25 @@ reddit = praw.Reddit(client_id='OlM6d2hKSrhbkw',
 print(reddit.user.me())
 
 user = raw_input("What user's comments would you like to scrape?\n")
-fileName = raw_input("What would you like to name your file? This is where you'll store all the data. Do not include the extension.\n")
+file = raw_input("What would you like to name your file? This is where you'll store all the data. Do not include the extension.\n")
 
-final_fname  = fileName + '.csv'
+#TESTING: default for testing 
+#user = "shaggorama"
+#fileName = "TEST"
+
+fileName  = file + '.csv'
 
 
-with open(final_fname,'w') as f1:
+with open(fileName,'w') as f1:
 	writer = csv.writer(f1, delimiter=',')
-	writer.writerow(["#","Comment","Timestamp(PT)", "Comment Score", "Number of Comments in Post"])
+	writer.writerow(["#","Comment","Timestamp(PT)", "Comment Score", "Number of Comments in Post", "Perpective Score"])
 	index = 0
-	for comment in reddit.redditor(user).comments.new(limit=1500):
+	for comment in reddit.redditor(user).comments.new(limit=20):
 		index += 1
-		row = [index,comment.body.encode('utf8'),datetime.datetime.fromtimestamp(int(comment.created_utc)).strftime('%Y-%m-%d %H:%M:%S').encode('utf8'), comment.score, comment.num_comments]
+		print comment.body
+		
+		perspectiveScore = runPerspective(comment.body)
+			
+		row = [index,comment.body.encode('utf8'),datetime.datetime.fromtimestamp(int(comment.created_utc)).strftime('%Y-%m-%d %H:%M:%S').encode('utf8'), comment.score, comment.num_comments, perspectiveScore]
 		writer.writerow(row)
 
