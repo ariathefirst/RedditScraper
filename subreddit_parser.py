f59aa7bca0ad9836bbc94b541788deeeaca1fbd1
N. Suj <nsujela@ucdavis.edu>
2018-03-14 19:53:06 -0700

    Pushing subreddit parser

--- subreddit_parser.py
+++ subreddit_parser.py
@@ -1,120 +1,125 @@
+# ---------------- SUBREDDIT --------------------
 # Get specified number of comments from subreddit
-
+# -----------------------------------------------
 import praw
 import psraw
+import sys
 import datetime
 import csv
 import json
 import googleapiclient
 from googleapiclient import discovery
+import codecs
 
+# ----------------------------------------------------------
+# Return Google Perspective score for a given string of text
+# ----------------------------------------------------------
 def runPerspective(str):
-	API_KEY='AIzaSyAlCwhKJ0C8n4eFM-ioPC5-MCFYy4P-TT8'
+    API_KEY='AIzaSyAlCwhKJ0C8n4eFM-ioPC5-MCFYy4P-TT8'
+    
+    # Generates API client object dynamically based on service name and version.
+    service = discovery.build('commentanalyzer', 'v1alpha1', developerKey=API_KEY)
 
-	# Generates API client object dynamically based on service name and version.
-	service = discovery.build('commentanalyzer', 'v1alpha1', developerKey=API_KEY)
+    analyze_request = {
+	   'comment': { 'text': str },
+	   'requestedAttributes': {'TOXICITY': {}}
+    }
 
-	analyze_request = {
-		'comment': { 'text': str },
-		'requestedAttributes': {'TOXICITY': {}}
-	}
-	try:
-		response = service.comments().analyze(body=analyze_request).execute()
-		json.dumps(response)
-		return response["attributeScores"]["TOXICITY"]["summaryScore"]["value"]
-		# print response.attributeScores.TOXICITY.summaryScore.value
-	except googleapiclient.errors.HttpError as e:
-		return "Error!"	
-
+    try:
+        response = service.comments().analyze(body=analyze_request).execute()
+        json.dumps(response)
+        # print response.attributeScores.TOXICITY.summaryScore.value
+        return response["attributeScores"]["TOXICITY"]["summaryScore"]["value"]
 	
+    except googleapiclient.errors.HttpError as e:
+        return "Error!"	
+
+# ----------------------------------------------------------
+#                          M A I N
+# ----------------------------------------------------------
+
+# Establish instance of reddit
 reddit = praw.Reddit(client_id='OlM6d2hKSrhbkw',
                      client_secret='w3kllzs-03WScaa9DFMHvRdTnQg',
                      password='WebW0rld',
                      user_agent='script by /u/sravyadivakarla123',
                      username='sravyadivakarla123')
 
-print(reddit.user.me())
+# Confirm successful instance initialization
+# print(reddit.user.me())
 
-userOrSubreddit = raw_input("Would you like to scrape a user or a subreddit? Type u for user, s for subreddit. \n")
+# Take in CSV file of provided subreddits to scrape
+# r --> read permissions only
+csvFile = 'subreddit_list.csv'
 
-# Ensure valid input
+all_subreddits = []
+
+with open(csvFile, 'rU') as f:
+    reader = csv.reader(f, delimiter=',')
+
+    # Each item is a list of containing all items in the row
+    for item in reader:
+        all_subreddits.append(item[0])
+        print(item[0])
+
+fileName = 'subreddit_dump.csv'
+
+# Add header
+with open(fileName, 'a') as csv_:
+    writer = csv.writer(csv_, delimiter=',')
+    writer.writerow(["#", "Subreddit", "Submission", "Comment", "Timestamp(PT)", "Comment Score", "Number of Comments in Submission", "Perpective Score"])
+
+# Traverse all subreddits and get comments from each one
+# Append them all to subreddit_dump.csv
+
+for subreddit in all_subreddits:    
+    with open(fileName,'a') as f1:
+        writer = csv.writer(f1, delimiter=',')
+        index = 0
+
+        maxNumComments = 1500
         
-if (userOrSubreddit == "u"):
-        user = raw_input("Which user's comments would you like to scrape?\n")
-
-elif (userOrSubreddit == "s"):
-        subreddit = raw_input("Which subreddit would you like to scrape?\n")
+        # Initialize bookkeeping variables
+        totalCommentCount = 0
+        subCount = 0
         
-file = raw_input("What would you like to name your file? This is where you'll store all the data. Do not include the extension.\n")
+        for submission in reddit.subreddit(subreddit).submissions():
+            if (totalCommentCount > 1):
+                    break
 
-fileName  = file + '.csv'
+            subCount += 1
+            print("subCount: ")
+            print(subCount)
+            print("\n")
 
-with open(fileName,'w') as f1:
-	writer = csv.writer(f1, delimiter=',')
-	index = 0
+            localCommentCount = 0
 
-        # Scraping User
-        if (userOrSubreddit == "u"):
-                writer.writerow(["#","Comment","Timestamp(PT)", "Comment Score", "Number of Comments in Post", "Perpective Score"])
-                for comment in reddit.redditor(user).comments.new(limit=20):
-		        index += 1
-		        print comment.body
-		
-		        perspectiveScore = runPerspective(comment.body)
-			
-		        row = [index,comment.body.encode('utf8'),datetime.datetime.fromtimestamp(int(comment.created_utc)).strftime('%Y-%m-%d %H:%M:%S').encode('utf8'),
-                               comment.score, comment.num_comments, perspectiveScore]
+            submission.comments.replace_more(limit=None)
+            
+            for comment in submission.comments.list():
+                totalCommentCount += 1
+                localCommentCount += 1
 
-                        writer.writerow(row)
+                print("Total comment count: ")
+                print(totalCommentCount)
+                print("\n")
 
-        # Scraping Subreddit
-        elif (userOrSubreddit == "s"):
+                print("Local comment count: ")
+                print(localCommentCount)
+                print("\n")
 
-                maxNumComments = 5000
+                print(submission.permalink)
+                print(comment.body)
+
+                perspectiveScore = runPerspective(comment.body)
                 
-                # Initialize bookkeeping variables
-                
-                totalCommentCount = 0
-                subCount = 0
+                # EACH ROW IN THE CSV CONTAINS:
+                # Subreddit name, submission link, index, comment body, timestamp, comment score, number of comments in submission,
+                # Google Perspective toxicity rating
 
-                writer.writerow(["#", "Subreddit", "Submission", "Comment", "Timestamp(PT)", "Comment Score", "Number of Comments in Submission", "Perpective Score"])
-                for submission in reddit.subreddit(subreddit).submissions():
+                row = [totalCommentCount, subreddit, submission.permalink, comment.body.encode('utf8'),
+                           datetime.datetime.fromtimestamp(int(comment.created_utc)).strftime('%Y-%m-%d %H:%M:%S').encode('utf8'), comment.score,
+                           submission.num_comments, perspectiveScore]
 
-                        if (totalCommentCount > maxNumComments):
-                                break
+                writer.writerow(row)
 
-                        subCount += 1
-                        print("subCount: ")
-                        print(subCount)
-                        print("\n")
-
-                        localCommentCount = 0
-
-                        submission.comments.replace_more(limit=None)
-                        
-                        for comment in submission.comments.list():
-                                totalCommentCount += 1
-                                localCommentCount += 1
-
-                                print("Total comment count: ")
-                                print(totalCommentCount)
-                                print("\n")
-
-                                print("Local comment count: ")
-                                print(localCommentCount)
-                                print("\n")
-
-                                print(submission.permalink)
-                                print(comment.body)
-
-		                perspectiveScore = runPerspective(comment.body)
-
-                                # EACH ROW IN THE CSV CONTAINS:
-                                # Subreddit name, submission link, index, comment body, timestamp, comment score, number of comments in submission,
-                                # Google Perspective toxicity rating
-
-                                row = [totalCommentCount, subreddit, submission.permalink, comment.body.encode('utf8'),
-                                       datetime.datetime.fromtimestamp(int(comment.created_utc)).strftime('%Y-%m-%d %H:%M:%S').encode('utf8'), comment.score,
-                                       submission.num_comments, perspectiveScore]
-
-                                writer.writerow(row)
